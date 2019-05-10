60b10f6df8c12191905c9ec12e3c4c09349bc75d
Kevin Wittek <kevin.wittek@codecentric.de>
2019-05-10 11:59:30 +0100

    Add JDBC URL example

---
+++ src/test/groovy/com/groovycoder/testcontainersexample/BookRepositoryJdbcSpec.groovy
@@ -0,0 +1,66 @@
+package com.groovycoder.testcontainersexample
+
+import org.flywaydb.core.Flyway
+import spock.lang.Specification
+
+class BookRepositoryJdbcSpec extends Specification {
+
+    static final TC_JDBC_URL = 'jdbc:tc:postgresql:9.6.8://hostname/databasename?TC_DAEMON=true'
+
+    def setup() {
+
+        def flyway = new Flyway()
+        flyway.setDataSource(TC_JDBC_URL,
+                '',
+                '')
+        flyway.migrate()
+    }
+
+    private BookRepository buildRepository() {
+        new BookRepository(TC_JDBC_URL,
+                '',
+                '')
+    }
+
+    def "empty repository is empty"() {
+        expect:
+        buildRepository().count() == 0
+    }
+
+    def "repository contains one book after saving it"() {
+        given: "the repo"
+        def repo = buildRepository()
+
+        and: "a book"
+        def book = new Book("Moby Dick", "Herman Melville")
+
+        when: "saving it"
+        repo.save(book)
+
+        then: "repo contains one book"
+        repo.count() == 1
+    }
+
+    def "repository finds books of given author"() {
+        given: "the repo"
+        def repo = buildRepository()
+
+        and: "some books"
+        def mobyDick = new Book("Moby Dick", "Herman Melville")
+        def terryPratchett = "Terry Pratchett"
+        def magic = new Book("The Colour of Magic", terryPratchett)
+        def elephant = new Book("The Fifth Elephant", terryPratchett)
+
+        when: "saving them"
+        [mobyDick, magic, elephant].each { repo.save(it) }
+
+        and: "searching for books of author"
+        def foundBooks = repo.findAllByAuthor(terryPratchett)
+
+        then: "expected books were found"
+        foundBooks.size() == 2
+        foundBooks.containsAll([magic, elephant])
+    }
+
+
+}
