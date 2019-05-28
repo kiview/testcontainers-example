a16f916c8a59d629b7df740d9578863180191d62
Kevin Wittek <kevin.wittek@styracosoft.de>
2019-05-28 18:12:17 +0200

    Add Kafka example for Jupiter

---
+++ src/test/java/com/groovycoder/testcontainersexample/SpecialOfferNotifierJupiterTests.java
@@ -0,0 +1,64 @@
+package com.groovycoder.testcontainersexample;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@Testcontainers
+public class SpecialOfferNotifierJupiterTests {
+
+    @Container
+    private KafkaContainer kafka = new KafkaContainer();
+
+    @Test
+    void notifierSendsRecordsToKafkaTopic() {
+        String topic = "notification";
+        SpecialOfferNotifier notifier = new SpecialOfferNotifier(kafka.getBootstrapServers(), "clientFoobar", topic);
+
+        // use consumer to check received records
+        KafkaConsumer<String, String> consumer = createConsumer(topic);
+
+        String notificationMessage = "foobar";
+        notifier.sendNotification(notificationMessage);
+
+
+        await().untilAsserted(() -> {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            assertFalse(records.isEmpty());
+            assertEquals(notificationMessage, records.iterator().next().value());
+        });
+
+    }
+
+    private KafkaConsumer<String, String> createConsumer(String topic) {
+        Map<String, Object> kafkaConfig = new HashMap<>();
+        kafkaConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        kafkaConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "tc-" + UUID.randomUUID());
+        kafkaConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        kafkaConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+        kafkaConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(kafkaConfig);
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+    }
+
+
+}
